7c448eb796d0c7ccd877228bd30d4fc026e4a9f1
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-06-10 13:46:11 +0000

    added need to fix Casino

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,17 +1,16 @@
 public class Shop {
     
-    protected Armor[] armor;
+    protected Armor armor;
     protected Weapon[] weapon;
     protected Potion[] potion;
 
-    public Shop(Armor[] armor, Weapon[] weapon, Potion[] potion) {
+    public Shop(Armor armor, Weapon[] weapon, Potion[] potion) {
         this.armor = armor;
         this.weapon = weapon;
         this.potion = potion;
     }
 
     public Shop() {
-        this.armor = new Armor[0];
         this.weapon = new Weapon[0];
         this.potion = new Potion[0];
     }
