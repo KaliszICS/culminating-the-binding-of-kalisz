488f90def3f7c6d9457b255532a561680f7bddd8
Karthikeya Arikirevula <karthikeya.arikirevula@student.tdsb.on.ca>
2025-06-12 01:31:55 +0000

    Entity Javadoc done

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -1,3 +1,7 @@
+/**
+ * Represents a character or unit in a battle with stats, moves, and effects.
+ * @author Jamin Xie
+ */
 public class Entity {
     String name;
     int maxHp;
@@ -9,6 +13,18 @@ public class Entity {
     int[] heal;
     int[] speedBuff;
 
+    /**
+     * Creates a new Entity with all required stats and moves.
+     *
+     * @param name       name of the entity
+     * @param maxHp      maximum HP
+     * @param speed      base speed
+     * @param status     current status 
+     * @param moveset    of move names
+     * @param damage     damage values for each move
+     * @param heal       healing values for each move
+     * @param speedBuff  speed buffs for each move
+     */
     public Entity(String name, int maxHp, int speed, String status, String[] moveset, int[] damage, int[] heal, int[] speedBuff) {
         this.name = name;
         this.maxHp = maxHp;
@@ -21,75 +37,132 @@ public class Entity {
         this.speedBuff = speedBuff;
     }
 
-    // Getters and setters
+    // 
+
+    /**
+     * @return  name of the entity
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return  max HP of the entity
+     */
     public int getMaxHp() {
         return maxHp;
     }
 
+    /**
+     * @param maxHp new max HP to set
+     */
     public void setMaxHp(int maxHp) {
         this.maxHp = maxHp;
     }
 
+    /**
+     * @return current HP
+     */
     public int getHp() {
         return hp;
     }
 
+    /**
+     * @param hp new HP to set
+     */
     public void setHp(int hp) {
         this.hp = hp;
     }
 
+    /**
+     * @return entity's speed
+     */
     public int getSpeed() {
         return speed;
     }
 
+    /**
+     * @param speed new speed to set
+     */
     public void setSpeed(int speed) {
         this.speed = speed;
     }
 
+    /**
+     * @return entity's current status
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * @param status new status to set
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * @return names of the moves this entity can use
+     */
     public String[] getMoveset() {
         return moveset;
     }
 
+    /**
+     * @param moveset new moveset to set
+     */
     public void setMoveset(String[] moveset) {
         this.moveset = moveset;
     }
 
+    /**
+     * @return damage values for each move
+     */
     public int[] getDamage() {
         return damage;
     }
 
+    /**
+     * @param damage new damage values to set
+     */
     public void setDamage(int[] damage) {
         this.damage = damage;
     }
 
+    /**
+     * @return healing values each of the moves
+     */
     public int[] getHeal() {
         return heal;
     }
 
+    /**
+     * @param heal new heal values to set
+     */
     public void setHeal(int[] heal) {
         this.heal = heal;
     }
 
+    /**
+     * @return speed buffs for each move
+     */
     public int[] getSpeedBuff() {
         return speedBuff;
     }
 
+    /**
+     * @param speedBuff new speed buffs to set
+     */
     public void setSpeedBuff(int[] speedBuff) {
         this.speedBuff = speedBuff;
     }
 
+    /**
+     * Adjusts the HP, Speed, Damage, Heal, and speedBuff of the entity, used by leveling up or powering up the enemy or player
+     *
+     * @param multiplier the factor to scale stats by so bosses get harder and stronger
+     */
     public void scaleStats(double multiplier) {
         this.maxHp = (int)(this.maxHp * multiplier);
         this.hp = this.maxHp;
@@ -108,6 +181,9 @@ public class Entity {
         }
     }
 
+    /**
+     * @return the string showing this entity's name, HP, speed, and status
+     */
     @Override
     public String toString() {
         return name + " | HP: " + hp + "/" + maxHp + " | Speed: " + speed + " | Status: " + status;
